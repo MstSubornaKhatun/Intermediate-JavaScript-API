4667aa4cbf484ded529f976891bb93de62da76bb
suborna33 <152749142+suborna33@users.noreply.github.com>
2026-01-06 07:42:04 +0600

    bssic es6

---
+++ m30/es6.js
@@ -0,0 +1,33 @@
+// console.log(name)
+// var name = "nahid";
+
+
+// const countryName = "bangladesh";
+
+
+
+// let age = 10;
+
+// age = 11;
+
+// console.log(age)
+
+
+const friends = ["hena", "hero alom"];
+friends.push("hamid")
+console.log(friends) // (3) ['hena', 'hero alom', 'hamid']
+
+
+for (let i = 0; i < friends.length; i++) {
+    const element = friends[i];
+    console.log(element)
+    
+}
+// console.log(friends)
+
+const person = {
+    name: "gias uddin",
+    
+}
+// person.name = "hsudhfbsi";
+// console.log(person)
