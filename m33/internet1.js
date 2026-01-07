894480061578ad7f8e3da10042d288f4f7b846e2
suborna33 <152749142+suborna33@users.noreply.github.com>
2026-01-07 10:18:10 +0600

    internet

---
+++ m33/internet1.js
@@ -0,0 +1,13 @@
+/*
+what is internet?
+what is is IP address?
+IPv4 vs IPv6
+what is DNS?
+steps of DNS
+how DNS works?
+how the internet works
+what is HTTP?
+HTTP Layer
+HTTP vs HTTPS
+what is an API?
+*/ 
